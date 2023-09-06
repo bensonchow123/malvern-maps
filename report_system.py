bddf4f301fb25d0d827ecb6d0feb723cd8639845
Benson Chow <email@bensonc.how>
2023-09-06 12:58:51 +0100

    finishing form objects

--- report_system.py
+++ report_system.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from forms import LoginForm
+from forms import LoginForm, RegisterForm, PasswordResetForm
 report_system = Blueprint("report_system", __name__)
 
 @report_system.route('/login', methods=['GET', 'POST'])
@@ -9,10 +9,16 @@ def login():
         pass
     return render_template('login.html', form=form)
 
-@report_system.route('/register')
+@report_system.route('/register',  methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('register.html', form=form)
 
-@report_system.route('/forgot_password')
-def forgot_password():
-    return render_template('forgot_password.html')
+@report_system.route('/password_reset',  methods=['GET', 'POST'])
+def password_reset():
+    form = PasswordResetForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('password_reset.html', form=form)
