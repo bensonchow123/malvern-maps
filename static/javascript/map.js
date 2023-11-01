b34efd3f5c6b18437b0a8f50a48db76abba007ae
Benson Chow <email@bensonc.how>
2023-11-01 23:46:17 +0000

    added map version 1

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -1,13 +1,22 @@
+var imageUrl = '/static/images/map.png';
+
+// Define the bounds of your image
+var imageBounds= [[0, 0], [1567, 1653]]; // Replace with the actual size of your image
+
+// Remove the tileLayer and add an imageOverlay
 var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13,
-    zoomControl: false
+    center: [0, 0],
+    zoom: 0,
+    minZoom: -5,
+    zoomControl: false,
+    crs: L.CRS.Simple
 });
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19
-}).addTo(map);
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 L.control.zoom({
     position: 'topright'
 }).addTo(map);
+
+// Set the view to the center of your image
+map.setView([imageBounds[1][1] / 2, imageBounds[1][0] / 2]);
