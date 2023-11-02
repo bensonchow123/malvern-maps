289f918495f76e1220020ec009d32cec9abe0f65
unknown <benson@benchowchunman.com>
2023-11-02 00:28:54 +0000

    fix overlaped zoom controls

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -14,13 +14,7 @@ var map = L.map('map', {
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 L.control.zoom({
-    position: 'topright'
+    position: 'bottomright'
 }).addTo(map);
 
 map.setView([imageBounds[1][1] / 2, imageBounds[1][0] / 2]);
-map.on('click', function(e) {
-    var coord = e.latlng;
-    var lat = coord.lat;
-    var lng = coord.lng;
-    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
-});
