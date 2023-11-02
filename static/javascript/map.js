9dbb71f98fe71db528464f5e26f9e05fd3c5a3cb
unknown <benson@benchowchunman.com>
2023-11-02 11:57:49 +0000

    fix moblie device not seeing complete map

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -4,8 +4,8 @@ var imageBounds= [[0, 0], [1567, 1653]];
 
 var map = L.map('map', {
     center: [890, 881],
-    zoom: 0,
-    minZoom: -1,
+    zoom: -1,
+    minZoom: -2,
     zoomControl: false,
     attributionControl: false,
     crs: L.CRS.Simple
@@ -13,10 +13,9 @@ var map = L.map('map', {
 
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
+
 L.control.zoom({
     position: 'bottomright'
 }).addTo(map);
 
 map.setView([imageBounds[1][1] / 2, imageBounds[1][0] / 2]);
-
-setTimeout(function(){ map.invalidateSize()}, 500);
