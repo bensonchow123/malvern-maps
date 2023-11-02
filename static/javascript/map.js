6d313b4e2a781aeb161c41a0dfbcefa156bdc54d
unknown <benson@benchowchunman.com>
2023-11-02 00:00:12 +0000

    tinkering with map peremeters

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -5,9 +5,9 @@ var imageBounds= [[0, 0], [1567, 1653]]; // Replace with the actual size of your
 
 // Remove the tileLayer and add an imageOverlay
 var map = L.map('map', {
-    center: [0, 0],
+    center: [890, 881],
     zoom: 0,
-    minZoom: -5,
+    minZoom: -1,
     zoomControl: false,
     crs: L.CRS.Simple
 });
@@ -20,3 +20,9 @@ L.control.zoom({
 
 // Set the view to the center of your image
 map.setView([imageBounds[1][1] / 2, imageBounds[1][0] / 2]);
+map.on('click', function(e) {
+    var coord = e.latlng;
+    var lat = coord.lat;
+    var lng = coord.lng;
+    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+});
