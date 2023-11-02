421294e042782fc7df99de178e8bfc7cfb42479b
unknown <benson@benchowchunman.com>
2023-11-02 00:04:22 +0000

    remove leaflet attribution

--- static/javascript/map.js
+++ static/javascript/map.js
@@ -1,14 +1,13 @@
 var imageUrl = '/static/images/map.png';
 
-// Define the bounds of your image
-var imageBounds= [[0, 0], [1567, 1653]]; // Replace with the actual size of your image
+var imageBounds= [[0, 0], [1567, 1653]];
 
-// Remove the tileLayer and add an imageOverlay
 var map = L.map('map', {
     center: [890, 881],
     zoom: 0,
     minZoom: -1,
     zoomControl: false,
+    attributionControl: false,
     crs: L.CRS.Simple
 });
 
@@ -18,7 +17,6 @@ L.control.zoom({
     position: 'topright'
 }).addTo(map);
 
-// Set the view to the center of your image
 map.setView([imageBounds[1][1] / 2, imageBounds[1][0] / 2]);
 map.on('click', function(e) {
     var coord = e.latlng;
